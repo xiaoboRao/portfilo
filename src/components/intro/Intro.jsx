7dfead05d3ef1cdffe232bb340f5417b280b145d
Bob <731653765@qq.com>
2021-12-02 23:25:08 +0800

    finish intro area
    finish intro area

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,39 @@
-import "./intro.scss"
+import React, { Component, useEffect, useRef } from "react";
+import { init } from "ityped";
+import "./intro.scss";
 
 export default function Intro() {
-    return (
-        <div className="intro" id="intro">
-            intro
+  const textRef = useRef();
+
+  console.log(textRef, "--------");
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      typeSpeed: 300,
+      backSpeed: 80,
+      backDelay: 2000,
+      strings: ["Front End Developer"],
+    });
+  }, []);
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img className="img" src="assets/man.png" alt="" />
         </div>
-    )
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There I'm</h2>
+          <h1> Bob</h1>
+          <h3>
+            <span ref={textRef}></span>{" "}
+          </h3>
+        </div>
+        <a href="#portfilo">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
 }
