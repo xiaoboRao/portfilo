8eb5534ecefac59dab242a578b7fc0ff6a18c6cf
xiaoboRao <731653765@qq.com>
2021-12-06 15:00:56 +0800

    fix work can not jump bug

--- src/components/componentData/menuData.js
+++ src/components/componentData/menuData.js
@@ -2,7 +2,7 @@ export const menuData = {
   data: [
     { name: "Home", href: "#intro" },
     { name: "portfilo", href: "#portfilo" },
-    { name: "work", href: "#work" },
+    { name: "works", href: "#work" },
     { name: "testimonials", href: "#testimonials" },
     { name: "contact", href: "#contact" },
   ],
